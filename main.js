666b2cd5bca92372794ea86ef9bba245c8147679
Francesco Sica <fsica171@gmail.com>
2021-03-10 17:16:00 +0100

    active

--- main.js
+++ main.js
@@ -1,15 +1,27 @@
+// var btn_menu = $("a > i.fa-bars");
+// var btn_menu_toogle = $("a > i.fa-times");
+//
+//
+// btn_menu.click(function() {
+//     var btn_Hamburger = $("div.hamburger-menu");
+//     btn_Hamburger.toggle("display");
+// });
+//
+// btn_menu_toogle.click(function() {
+//     var btn_Hamburger = $("div.hamburger-menu");
+//     btn_Hamburger.toggle("display");
+// });
+
 var btn_menu = $("a > i.fa-bars");
+var btn_menu_toogle = $("a > i.fa-times");
+
 
 btn_menu.click(function() {
     var btn_Hamburger = $("div.hamburger-menu");
-    btn_Hamburger.toggle("display");
-
+    btn_Hamburger.toggle("active");
 });
-
-var btn_menu_toogle = $("a > i.fa-times");
 
 btn_menu_toogle.click(function() {
     var btn_Hamburger = $("div.hamburger-menu");
-    btn_Hamburger.toggle("display");
-
+    btn_Hamburger.toggle("active");
 });
